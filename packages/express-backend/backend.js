67e2481a4c07f029351c2ac33c0b6dad24ffbc63
JOHNDRAJ <95675004+JOHNDRAJ@users.noreply.github.com>
2024-10-10 23:19:54 -0700

    finished assignment

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -32,7 +33,7 @@ const users = {
       }
     ]
   };
-
+app.use(cors());
 app.use(express.json());
 
 const findUserByName = (name) => {
@@ -84,19 +85,24 @@ if (name != undefined) {
 });
 
 app.get("/users/:id", (req, res) => {
-const id = req.params["id"]; //or req.params.id
-let result = findUserById(id);
-if (result === undefined) {
-    res.status(404).send("Resource not found.");
-} else {
-    res.send(result);
-}
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
 });
   
 app.post("/users", (req, res) => {
-const userToAdd = req.body;
-addUser(userToAdd);
-res.send();
+    const userToAdd = req.body;
+    if (userToAdd && userToAdd.name && userToAdd.job) {
+        userToAdd.id = Math.floor(100000 + Math.random() * 900000).toString()
+        addUser(userToAdd);
+        res.status(201).json(userToAdd); 
+    } else {
+        res.status(400).send("Invalid user data");
+    }
 });
 
 app.get("/", (req, res) => {
@@ -104,7 +110,7 @@ app.get("/", (req, res) => {
 });
 
 app.delete("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
+    const id = req.params["id"];
     const userIndex = users["users_list"].findIndex((user) => user["id"] === id);
     if (userIndex === -1) {
         res.status(404).send("Resource not found.");
